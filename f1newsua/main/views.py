8c7c33c4c7628592935cd0d5c43d45a68978364d
TexnoViktor <viktor.ushakov44@gmail.com>
2023-05-27 01:34:48 +0200

    refactoring code

--- f1newsua/main/views.py
+++ f1newsua/main/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 # Create your views here.
 
+
 def index(request):
     return render(request, 'main/index.html')
+
+
+def drivers(request):
+    return render(request, 'main/drivers.html')
+
+
+def schedule(request):
+    return render(request, 'main/schedule.html')
+
+
+def standings(request):
+    return render(request, 'main/standings.html')
+
+
+def teams(request):
+    return render(request, 'main/teams.html')
