f859d14fe6c428e80c5505ba3e4a8f34ee9653c4
TexnoViktor <viktor.ushakov44@gmail.com>
2023-05-29 00:06:04 +0200

    refactoring code

--- f1newsua/main/urls.py
+++ f1newsua/main/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('drivers', views.drivers),
+    path('teams', views.teams),
+    path('schedule', views.schedule),
+    path('standings', views.standings),
 ]
