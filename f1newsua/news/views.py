044e7c0c0d9bc47031e631ac7c1f20fe15c97342
TexnoViktor <viktor.ushakov44@gmail.com>
2023-05-30 00:16:12 +0200

    added subscribe webapp + little correction to news

--- f1newsua/news/views.py
+++ f1newsua/news/views.py
@@ -12,3 +12,14 @@ class NewsDetailView(DetailView):
     model = Article
     template_name = 'news/example.html'
     context_object_name = 'article'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        obj = self.get_object()
+        text_length = len(obj.full_text)
+        half_length = text_length // 2
+        first_half_text = obj.full_text[:half_length]
+        second_half_text = obj.full_text[half_length:]
+        context['first_half_text'] = first_half_text
+        context['second_half_text'] = second_half_text
+        return context
