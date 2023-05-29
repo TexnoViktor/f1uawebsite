25828773cf828cd52086be10c3b3c2e9bae8d407
TexnoViktor <viktor.ushakov44@gmail.com>
2023-05-29 18:01:43 +0200

    rewritten html pages to work with template

--- f1newsua/main/urls.py
+++ f1newsua/main/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index),
-    path('drivers', views.drivers),
-    path('teams', views.teams),
-    path('schedule', views.schedule),
-    path('standings', views.standings),
+    path('', views.index, name='home'),
+    path('drivers', views.drivers, name='drivers'),
+    path('teams', views.teams, name='teams'),
+    path('schedule', views.schedule, name='schedule'),
+    path('standings', views.standings, name='standings'),
 ]
