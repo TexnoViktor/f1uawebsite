a600445e635ce04920342d19b396b73fc65adeac
Viktor <ushakovviktor14@gmail.com>
2023-12-26 03:01:50 +0100

    Adding testing

--- f1newsua/main/tests.py
+++ f1newsua/main/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from news.models import Article
 
-# Create your tests here.
+class MainViewsTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Створення тестових статей для перевірки відображення на головній сторінці
+        for i in range(5):
+            Article.objects.create(
+                title=f'Test Article {i+1}',
+                full_text=f'This is a test article {i+1}.'
+            )
+
+    def test_index_view(self):
+        client = Client()
+        response = client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/index.html')
+        # Перевірка, що статті передаються у контексті
+        self.assertTrue('photo1' in response.context)
+        self.assertTrue('photo2' in response.context)
+        self.assertTrue('photo3' in response.context)
+        self.assertTrue('photo4' in response.context)
+        self.assertTrue('photo5' in response.context)
+
+    def test_drivers_view(self):
+        client = Client()
+        response = client.get(reverse('drivers'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/drivers.html')
+
+    def test_schedule_view(self):
+        client = Client()
+        response = client.get(reverse('schedule'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/schedule.html')
+
+    def test_standings_view(self):
+        client = Client()
+        response = client.get(reverse('standings'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/standings.html')
+
+    def test_teams_view(self):
+        client = Client()
+        response = client.get(reverse('teams'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/teams.html')
--- f1newsua/news/tests.py
+++ f1newsua/news/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Article
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-# Create your tests here.
+class NewsViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Створення тестових статей
+        number_of_articles = 5
+        for article_id in range(number_of_articles):
+            Article.objects.create(
+                title=f'Article {article_id}',
+                full_text='This is a test article'
+            )
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/news/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('news'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'news/news.html')
+
+
+    def test_lists_all_articles(self):
+        response = self.client.get(reverse('news'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(len(response.context['news_arr']) == 5)
+
+
+class ArticleDetailViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Створення тестової статті з усіма необхідними полями
+        cls.article = Article.objects.create(
+            title='Test Article',
+            anons='Test Anons',
+            full_text='This is a full text of the article. It is quite long.',
+            photolink_1=SimpleUploadedFile('test_photo1.jpg', content=b'', content_type='image/jpeg'),
+            photolink_2=SimpleUploadedFile('test_photo2.jpg', content=b'', content_type='image/jpeg')
+        )
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('news-detail', args=[str(self.article.id)]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'news/example.html')
+
+    def test_context_data(self):
+        response = self.client.get(reverse('news-detail', args=[str(self.article.id)]))
+        self.assertTrue('first_half_text' in response.context)
+        self.assertTrue('second_half_text' in response.context)
+        full_text = self.article.full_text
+        self.assertEqual(response.context['first_half_text'], full_text[:len(full_text)//2])
+        self.assertEqual(response.context['second_half_text'], full_text[len(full_text)//2:])
